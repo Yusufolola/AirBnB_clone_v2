29f68a70881612b47a9d0b58fa38e53b2aa48906
yusufolola <yusufoyesanmi@yahoo.com>
2024-05-19 21:24:14 +0100

    default

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""hello routh"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """displays hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """display  “HBNB”"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_params(text):
+    """display 'C' followed by the value of the text
+    variable (replace underscore _ symbols with a space re"""
+    no_underscore = text.replace("_", " ")
+    return f"C {no_underscore}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    no_underscore = text.replace("_", " ")
+    return f"python {no_underscore}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
