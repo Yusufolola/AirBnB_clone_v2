f8955b165faa023183a800dff626cae268116542
yusufolola <yusufoyesanmi@yahoo.com>
2024-05-19 14:44:51 +0100

    sup

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,13 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route("/",strict_slashes=False)
+
+
+@app.route("/", strict_slashes=False)
 def hello():
     """displays hello hbnb"""
     return "Hello HBNB!"
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+if "__name__" == "__main__":
+    app.run(host="0.0.0.0", debug=True)
