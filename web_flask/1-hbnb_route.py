7230866a66215ce5184a04b5ca345a0a1a265adb
yusufolola <yusufoyesanmi@yahoo.com>
2024-05-19 14:52:21 +0100

    space

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,7 +4,17 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.route("/",strict_slashes=False)
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """displays hello hbnb"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """display  “HBNB”"""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
